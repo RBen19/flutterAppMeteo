6369dc4d43bee2a14125f15e5f5cff278d872c7a
RBen19 <bonmerciel@gmail.com>
2025-03-10 19:14:28 +0100

    update home screen

--- lib/Screen/home_screen.dart
+++ lib/Screen/home_screen.dart
@@ -12,7 +12,10 @@ class HomeScreen extends StatelessWidget {
         SizedBox(
           height: 280,
         ),
-        AnimatedScreen()
+        AnimatedScreen(),
+        SizedBox(
+          height: 80,
+        ),
       ]),
     );
   }
@@ -24,26 +27,55 @@ class AnimatedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: 200,
+      width: 400,
       decoration: BoxDecoration(),
-      child: Center(
-        child: AnimatedTextKit(
-            repeatForever: true,
-            pause: Duration(seconds: 2),
-            animatedTexts: [
-              TypewriterAnimatedText("Bienvenue dans votre app ...",
-                  textAlign: TextAlign.center,
-                  textStyle: BlueStyleHomeScreen()),
-              ScaleAnimatedText("M2 météo",
-                  textAlign: TextAlign.center,
-                  textStyle: OrangeStyleHomeScreen()),
-              FadeAnimatedText("Ciquer sur le button magique",
-                  textAlign: TextAlign.center,
-                  textStyle: BlueStyleHomeScreen()),
-              ColorizeAnimatedText('Pour commencer l\'experience',
-                  textAlign: TextAlign.center,
-                  textStyle: OrangeStyleHomeScreen(),
-                  colors: [Colors.orange, Colors.indigo]),
-            ]),
+      child: Stack(
+        children: [
+          Center(
+            child: Column(
+              children: [
+                AnimatedTextKit(
+                    repeatForever: true,
+                    pause: Duration(seconds: 2),
+                    animatedTexts: [
+                      TypewriterAnimatedText("Bienvenue dans votre app",
+                          textAlign: TextAlign.center,
+                          textStyle: BlueStyleHomeScreen()),
+                      ScaleAnimatedText("M2 météo",
+                          textAlign: TextAlign.center,
+                          textStyle: OrangeStyleHomeScreen()),
+                      FadeAnimatedText("Cliquez sur le button magique",
+                          textAlign: TextAlign.center,
+                          textStyle: BlueStyleHomeScreen()),
+                      ColorizeAnimatedText('Pour commencer l\'experience',
+                          textAlign: TextAlign.center,
+                          textStyle: OrangeStyleHomeScreen(),
+                          colors: [Colors.orange, Colors.indigo]),
+                    ]),
+                SizedBox(
+                  height: 100,
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: ElevatedButton(
+              onPressed: () {
+                print("redirection vers la page de la meteo");
+              },
+              child: AnimatedTextKit(repeatForever: true, animatedTexts: [
+                ColorizeAnimatedText('Cliquez moi',
+                    textAlign: TextAlign.center,
+                    textStyle: OrangeStyleHomeScreen(),
+                    colors: [Colors.blueAccent, Colors.orange])
+              ]),
+            ),
+          ),
+        ],
       ),
     );
   }
