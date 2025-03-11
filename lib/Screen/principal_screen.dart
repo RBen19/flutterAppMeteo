0592a10cd0a2898aa1ebae65594eff80f483aeb5
MiralAmana <charlizeamana@gmail.com>
2025-03-11 08:52:06 +0000

    Added animated linear  progress bar and dynamic waiting messages

--- lib/Screen/principal_screen.dart
+++ lib/Screen/principal_screen.dart
@@ -1,19 +1,63 @@
-import 'package:app_meteo/Screen/home_screen.dart';
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-class PrincipalScreen extends StatelessWidget {
-  const PrincipalScreen({super.key});
+class PrincipalScreen extends StatefulWidget {
+  @override
+  _PrincipalScreenState createState() => _PrincipalScreenState();
+}
+
+class _PrincipalScreenState extends State<PrincipalScreen> {
+  double _progress = 0.0;
+  int _messageIndex = 0;
+  final List<String> _messages = [
+    "Nous téléchargeons les données…",
+    "C’est presque fini…",
+    "Plus que quelques secondes avant d’avoir le résultat…"
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _startLoading();
+  }
+
+  void _startLoading() {
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      if (_progress < 1.0) {
+        setState(() {
+          _progress += 0.2;
+          _messageIndex = (_messageIndex + 1) % _messages.length;
+        });
+      } else {
+        timer.cancel();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-            onPressed: () {
-              Get.off(HomeScreen());
-            },
-            icon: Icon(Icons.arrow_back)),
+      backgroundColor: Colors.blueGrey[900],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SpinKitWave(color: Colors.white, size: 50.0), // Animation de chargement
+            SizedBox(height: 20),
+            LinearProgressIndicator(
+              value: _progress,
+              backgroundColor: Colors.grey[700],
+              color: Colors.blueAccent,
+              minHeight: 8,
+            ),
+            SizedBox(height: 20),
+            Text(
+              _messages[_messageIndex],
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            ),
+          ],
+        ),
       ),
     );
   }
