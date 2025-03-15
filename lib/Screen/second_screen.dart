a07bcafc20d341247464d9b66dfede1289d5d0ea
RBen19 <bonmerciel@gmail.com>
2025-03-15 22:57:58 +0100

    create card_villes_widget

--- lib/Screen/second_screen.dart
+++ lib/Screen/second_screen.dart
@@ -1,5 +1,7 @@
 import 'package:app_meteo/Screen/loader_screen.dart';
 import 'package:app_meteo/models/weather.dart';
+import 'package:app_meteo/utils/utils_fonctions.dart';
+import 'package:app_meteo/widgets/card_villes_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -19,8 +21,9 @@ class SecondScreen extends StatelessWidget {
       body: Center(
         child: Column(
           children: [
+            Utils.animation_builder("assets/animations/world.json", 200, 200),
             SizedBox(
-              height: 150,
+              height: 30,
             ),
             ElevatedButton(
               onPressed: () {
@@ -33,7 +36,68 @@ class SecondScreen extends StatelessWidget {
                     fontSize: 34,
                     color: Colors.indigo),
               ),
+            ),
+            SizedBox(
+              height: 35,
+            ),
+            /*
+            Container(
+              height: 180,
+              width: 420,
+              decoration: BoxDecoration(
+                  color: Colors.lightBlueAccent,
+                  borderRadius: BorderRadius.circular(20)),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text("${listWeather[0].current.temp_c}",
+                              style:
+                                  TextStyle(fontSize: 60, color: Colors.white)),
+                          Text(
+                            "degrés",
+                            style: TextStyle(
+                                color: Colors.deepOrange, fontSize: 20),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            "${listWeather[0].location.name}",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  /*
+                     Column(
+                    children: [Text("Temperature")],
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Column(
+                    children: [Text("Vitesse du vent")],
+                  )
+                 * */
+                ],
+              ),
             )
+           *
+           * */
+            CardVillesWidget()
           ],
         ),
       ),
---
+++ lib/widgets/card_villes_widget.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class CardVillesWidget extends StatelessWidget {
+  final nameTown;
+  final country;
+  final temp_c;
+  final wind;
+  const CardVillesWidget(
+      {super.key, this.nameTown, this.country, this.temp_c, this.wind});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.all(14.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Text(
+                  "${nameTown}",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueGrey[700],
+                  ),
+                ),
+                SizedBox(width: 6),
+                Text(
+                  "${country}",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.blueGrey[500],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "${temp_c}",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.orangeAccent[700],
+                  ),
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.air,
+                      color: Colors.blueAccent,
+                      size: 30,
+                    ),
+                    SizedBox(width: 6),
+                    Text(
+                      '${wind}',
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.blueGrey[600],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
